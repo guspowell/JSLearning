74dc8676f3bbe77f62f8929fd2625ec5bca196b2
guspowell <augustuspowell@me.com>
2015-03-30 15:11:32 +0100

    another way of iterating an array. anonymous functions

--- lecture33/iteratingArray.js
+++ lecture33/iteratingArray.js
@@ -3,3 +3,15 @@ array[10] = 11;
 for(var i=0; i<array.length; i++) {
   console.log(array[i]);
 }
+
+var array = [1,2,3]
+array.forEach( function(i) { console.log(i) })
+// 1
+// 2
+// 3
+
+function printElement(elem) { console.log(elem) }
+array.forEach(printElement)
+// 1
+// 2
+// 3
