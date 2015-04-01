450cdf404820b31925dc2cb739f4ffc2dd272ec9
guspowell <augustuspowell@me.com>
2015-04-01 18:52:11 +0100

    learning about the dom

---
+++ lecture60/DOM.js
@@ -0,0 +1,11 @@
+document.getElementById("logo")
+
+document.getElementById("header").childNodes[1]
+
+document.getElementByID("header").firstChild
+
+document.getElementByID("header").lastChild
+
+document.getElementByID("header").nextSibling/previousSibling
+
+document.getElementsByTagName("img") // => returns an array
